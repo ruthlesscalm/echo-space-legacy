e11aa0b650306dcb77d660ff45b041c34ae18fce
Ruthless Calm <ruthlesscalm@proton.me>
2025-12-14 17:51:46 +0530

    feat: add Tailwind CSS setup and update stylesheets

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./public/**/*.{html,js}", "./views/**/*.{ejs,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
