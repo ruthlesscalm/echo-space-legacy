b6618c7cf9313a56dd1b25bc8f49226fd8620a63
Ruthless Calm <ruthlesscalm@proton.me>
2025-12-17 17:39:46 +0530

    feat: add login and register pages with responsive design and improved accessibility

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "register.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
