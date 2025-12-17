6111af10b3f128feeca96c8341d1cefb205f4dfe
Ruthless Calm <ruthlesscalm@proton.me>
2025-12-17 20:46:51 +0530

    feat: add upload page with form for song and thumbnail uploads

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-const PORT = 3000;
 const app = express();
+const PORT = 3000;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -11,14 +11,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+})
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "login.html"));
-});
+})
 app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "register.html"));
-});
+})
+app.get("/upload", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "upload.html"));
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log("Server is live on http://localhost:3000");
+})
